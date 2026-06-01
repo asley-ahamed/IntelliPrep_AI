3db25fec47bc259cf3f6ad11c122a970decca9c9
asley-ahamed <asleyahamed0321@gmail.com>
2026-06-01 16:23:19 +0530

    Added Database Connection

--- IntelliPre_AI/src/mySqlConnection/dbConnection.java
+++ IntelliPre_AI/src/mySqlConnection/dbConnection.java
@@ -1,5 +1,31 @@
 package mySqlConnection;
 
-public class dbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class dbConnection 
+{
+    private static final String URL = "jdbc:mysql://localhost:3306/intelliPrepAi_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Asley@12@";
+
+    public static Connection getConnection()
+    {
+        Connection connection = null;
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database Connected Successfully");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
