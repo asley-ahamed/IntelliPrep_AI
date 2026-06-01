6fbd3b3ed3e5f93bfa5d6d23607079a6217a8db2
asley-ahamed <asleyahamed0321@gmail.com>
2026-06-01 16:43:37 +0530

    Added Database Connection

--- IntelliPre_AI/src/mySqlConnection/dbConnection.java
+++ IntelliPre_AI/src/mySqlConnection/dbConnection.java
@@ -7,7 +7,7 @@ public class dbConnection
 {
     private static final String URL = "jdbc:mysql://localhost:3306/intelliPrepAi_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "Asley@12@";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection()
     {
