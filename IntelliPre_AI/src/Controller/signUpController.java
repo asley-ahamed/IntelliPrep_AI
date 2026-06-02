f2c239633c4917431e6ba239dd6440ec856d214f
asley-ahamed <asleyahamed0321@gmail.com>
2026-06-02 09:51:29 +0530

    Implemented SignUp Controller

---
+++ IntelliPre_AI/src/Controller/signUpController.java
@@ -0,0 +1,82 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import Model.loginModel;
+import Model.signUpModel;
+import View.loginView;
+import View.signUpView;
+
+public class signUpController
+{
+	private signUpModel model;
+	private signUpView view;
+	
+	public signUpController(signUpModel model,signUpView view)
+	{
+		this.model=model;
+		this.view=view;
+		this.view.signUpButtonListener(new SignUpButtonListener());
+	}
+	
+	class SignUpButtonListener implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e) 
+		{
+			try
+			{
+				String email = view.getTxtEmail().getText().trim();
+				String username = view.getTxtUsername().getText().trim();
+				String password = String.valueOf(view.getTxtPassword().getPassword()).trim();
+				String confirmPassword = String.valueOf(view.getTxtConfirmPassword().getPassword()).trim();
+	 			
+	 			if(email.isEmpty() || username.isEmpty()|| password.isEmpty()|| confirmPassword.isEmpty())
+	 			{
+	 				JOptionPane.showMessageDialog(view,"Please Field All fields Before SignUp");
+	 			}
+	 			else if(!email.matches("^[A-Za-z0-9+_.-]+@(.+)$"))
+	 			{
+	 			    JOptionPane.showMessageDialog(view,"Please enter a valid email address");
+	 			    
+	 			}
+	 			else if(!password.equals(confirmPassword))
+	 			{
+	 				JOptionPane.showMessageDialog(view,"Passwords do not match. Please try again");
+	 			}
+	 			else 
+	 			{
+	 				  model.setEmail(email);
+	                    model.setUsername(username);
+	                    model.setPassword(password);
+					
+					if(model.addUser())
+					{
+						JOptionPane.showMessageDialog(view, "Account Create Succesfully");
+						view.dispose();
+						loginView view=new loginView();
+						loginModel model=new loginModel();
+						new loginController(model, view);
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(view,"Duplicate Username or Email");
+					}
+				}
+				
+			} catch (Exception e2) 
+			{
+				JOptionPane.showMessageDialog(view, e2);
+			}
+ 			
+ 			
+
+			
+		}
+	}
+	
+
+}
